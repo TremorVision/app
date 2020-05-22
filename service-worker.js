0ae2ea107b4a57ec9484a56f035db45314d39391
Janae Chan <jchan15@cs.washington.edu>
2020-05-21 17:18:40 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app/precache-manifest.35ce570a0fa7689b9aacd856d617a03f.js"
+  "/app/precache-manifest.f5750da962a0b8f9f21abe4d40649fe3.js"
 );
 
 self.addEventListener('message', (event) => {
