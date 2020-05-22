a14c64d688e29f83f55478713c9b24b573984bdc
Janae Chan <jchan15@cs.washington.edu>
2020-05-22 12:03:12 -0700

    Updates

--- precache-manifest.abcc7576a7c337cb2d8fc717b8d4a7a8.js
+++ precache-manifest.abcc7576a7c337cb2d8fc717b8d4a7a8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97b1eb1aab951fc5091b049f8fae5dba",
+    "revision": "170cfaabcabab6bf0ac5b5aebdb779d2",
     "url": "/app/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/app/static/css/2.e4796f11.chunk.css"
   },
   {
-    "revision": "4f9a1614a176362ade79",
+    "revision": "9b3e0cc5cd092b1d14d5",
     "url": "/app/static/css/main.34cb0bed.chunk.css"
   },
   {
@@ -20,12 +20,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/app/static/js/2.8c537f26.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4f9a1614a176362ade79",
-    "url": "/app/static/js/main.69ee1884.chunk.js"
+    "revision": "9b3e0cc5cd092b1d14d5",
+    "url": "/app/static/js/main.430ee62e.chunk.js"
   },
   {
     "revision": "8d0d562f4ec3c131ac27d52de65ecd17",
-    "url": "/app/static/js/main.69ee1884.chunk.js.LICENSE.txt"
+    "url": "/app/static/js/main.430ee62e.chunk.js.LICENSE.txt"
   },
   {
     "revision": "acc57dc7dc2dd0d27129",
